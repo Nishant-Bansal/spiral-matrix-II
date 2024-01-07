9529677f82e22db6a4797d426090827bf67955ee
Nishant Bansal <47652694+Nishant-Bansal@users.noreply.github.com>
2024-01-07 14:56:48 +0530

    Time: 75 ms (33.33%) | Memory: 55.2 MB (39.39%) - LeetSync

--- 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.js
+++ 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.js
@@ -18,40 +18,8 @@ var maxAbsValExpr = function(arr1, arr2) {
     resultArr.push(Math.max(...a3)-Math.min(...a3))
     resultArr.push(Math.max(...a4)-Math.min(...a4))
 
-
     return Math.max(...resultArr)
 
-
-
-
-
-
-    // let max1 = Number.MIN_VALUE;
-    // let max2 = Number.MIN_VALUE;
-    // let max3 = Number.MIN_VALUE;
-    // let max4 = Number.MIN_VALUE;
-    // let min1 = Number.MAX_VALUE;
-    // let min2 = Number.MAX_VALUE;
-    // let min3 = Number.MAX_VALUE;
-    // let min4 = Number.MAX_VALUE;
-
-    // let len = arr1.length
-
-    // for(let i=0; i < len; i++){
-    //     max1 = Math.max(max1, (arr1[i] + arr2[i] + i))
-    //     min1 = Math.min(min1, (arr1[i] + arr2[i] + i))
-
-    //     max2 = Math.max(max2, (-arr1[i] + arr2[i] + i))
-    //     min2 = Math.min(min2, (-arr1[i] + arr2[i] + i))
-
-    //     max3 = Math.max(max3, (arr1[i] - arr2[i] + i))
-    //     min3 = Math.min(min3, (arr1[i] - arr2[i] + i))
-
-    //     max4 = Math.max(max4, (-arr1[i] - arr2[i] + i))
-    //     min4 = Math.min(min4, (-arr1[i] - arr2[i] + i)) 
-    // }
-
-    // return Math.max(Math.max(max1-min1, max2-min2), Math.max(max3-min3, max4-min4));
 };
 
 console.log("maxAbsValExpr ==> ", maxAbsValExpr([1,-2], [8, 8]))
