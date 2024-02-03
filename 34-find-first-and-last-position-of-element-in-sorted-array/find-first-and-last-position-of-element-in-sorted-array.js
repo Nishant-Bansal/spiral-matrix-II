a829207e9a01bcee2bc127b3ae112e43f1d24c34
Nishant Bansal <47652694+Nishant-Bansal@users.noreply.github.com>
2024-02-03 17:05:16 +0530

    Time: 48 ms (83.68%) | Memory: 49.6 MB (15.71%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -28,31 +28,4 @@ var searchRange = function(nums, target) {
     let right = binarySearch(nums, target, false)
 
     return [left, right]
-    // let len = nums.length;
-    // let ans = [];
-
-    // if(len===0) return [-1, -1];
-    // if(len === 1 && target === nums[0]){
-    //     return [0, 0]
-    // } else if(len === 1 && target !== nums[0]){
-    //     return [-1, -1]
-    // }
-
-    // let left = 0, right = len-1;
-
-    // if(nums.includes(target)){
-    //     while(left<=right){
-    //         let mid = Math.floor(left + (right-left)/2);
-
-    //         if(nums[mid] === target){
-    //             if(nums[mid-1] === target) ans.push(mid-1, mid)
-    //             else if(nums[mid+1] === target) ans.push(mid, mid+1)
-    //             else ans.push(mid, mid)
-    //             return ans;
-    //         } else if(nums[mid]>target){
-    //             right = mid-1;
-    //         } else left = mid+1
-    //     }
-    // } else return [-1, -1]
-    // return ans;
 };
